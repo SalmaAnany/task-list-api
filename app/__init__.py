5a1986a7ed4250ed04e5ab03e451a2ff3cfef34a
Salma Anany <salmayousry5@gmail.com>
2024-11-07 17:09:27 -0800

    wave 5 completed

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .db import db, migrate
 from .models import task, goal
 import os
 
+from .routes.goal_routes import goal_bp
 from .routes.task_routes import tasks_bp
 from dotenv import load_dotenv
 load_dotenv()
@@ -21,6 +22,7 @@ def create_app(config=None):
     db.init_app(app)
     migrate.init_app(app, db)
     app.register_blueprint(tasks_bp)
+    app.register_blueprint(goal_bp)
 
     # Register Blueprints here
 
--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -1,1 +1,89 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, abort, request
+
+from app import db
+from app.models.goal import Goal
+
+goal_bp = Blueprint("goal_bp",__name__, url_prefix="/goals")
+
+
+@goal_bp.post("")
+def create_goal():
+    request_body = request.get_json()
+    if "title" not in request_body.keys():
+        abort(make_response({"details": "Invalid data"}, 400))
+    title = request_body["title"]
+    new_goal = Goal(title=title)
+    db.session.add(new_goal)
+    db.session.commit()
+    response = {
+        "goal": new_goal.to_dict()
+    }
+    return response, 201
+
+@goal_bp.post("/<goal_id>")
+def create_goal_with_id(goal_id):
+    request_body = request.get_json()
+    if "title" not in request_body.keys():
+        abort(make_response({"details": "Invalid data"}, 400))
+    title = request_body["title"]
+    new_goal = Goal(id= goal_id, title=title)
+    db.session.add(new_goal)
+    db.session.commit()
+    response = {
+        "goal": new_goal.to_dict()
+    }
+    return response, 201
+@goal_bp.get("")
+def get_all_tasks():
+    title_param = request.args.get("title")
+
+    query = db.select(Goal)
+    if title_param:
+        query = query.where(Goal.title.like(f"%{title_param}"))
+
+    goals = db.session.scalars(query).all()
+    goals_response = [goal.to_dict() for goal in goals]
+    return goals_response
+
+def goal_id_validation(id):
+    if not id.isnumeric():
+        abort(make_response({"details": f" {id} Invalid data"}, 400))
+
+@goal_bp.get("/<goal_id>")
+def get_one_goal(goal_id):
+    goal_id_validation(goal_id)
+    query = db.select(Goal).where(Goal.id == goal_id)
+    goal = db.session.scalar(query)
+
+    if not goal:
+        response = {"message": f"{goal_id} not found"}
+        abort(make_response(response,404))
+    return {"goal": goal.to_dict()}
+
+@goal_bp.put("/<goal_id>")
+def update_goal(goal_id):
+    goal_id_validation(goal_id)
+    query = db.select(Goal).where(Goal.id == goal_id)
+    goal = db.session.scalar(query)
+
+    if goal is None:
+        abort(make_response({"details": f"Goal {goal_id} not found"}, 404))
+
+    request_body = request.get_json()
+    goal.title = request_body["title"]
+    db.session.commit()
+    return {
+        "task": goal.to_dict()
+    }
+
+@goal_bp.delete("/<goal_id>")
+def delete_task(goal_id):
+    goal_id_validation(goal_id)
+    query = db.select(Goal).where(Goal.id == goal_id)
+    goal = db.session.scalar(query)
+    if not goal:
+        response = {"details": f"{goal_id}Build a habit of going outside daily\" successfully deleted"}
+        abort(make_response(response,404))
+
+    db.session.delete(goal)
+    db.session.commit()
