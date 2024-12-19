2c87abb06b93f31624db3cb48ba01c52030739aa
Salma Anany <salmayousry5@gmail.com>
2024-12-19 01:34:47 -0800

    Add CORS

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,17 @@ from .db import db, migrate
 from .models import task, goal
 from .routes.goal_routes import goal_bp
 from .routes.task_routes import tasks_bp
+from flask_cors import CORS
 
 load_dotenv()
 
 
 def create_app(config=None):
     app = Flask(__name__)
+    CORS(app)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
